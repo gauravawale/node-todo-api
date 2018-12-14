b34cd9f6d7f5dabcba009e734b3a17397f96bcb7
Awale, Gaurav <Gaurav.Awale@cdk.com>
2018-12-14 21:24:54 +0530

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 let express = require('express');
 let bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 let {mongoose} = require('./db/mongoose');
 let {Todo} = require('./models/todo');
@@ -31,6 +32,22 @@ app.get('/todos', (req, res) => {
        res.status(400).send(err);
    }) ;
 });
+
+
+app.get('/todos/:id', (req, res) => {
+    let id = req.params.id;
+    if (!ObjectID.isValid(id))
+        res.status(404).send();
+    else {
+        Todo.findById(id).then((todo) => {
+            if (!todo)
+                res.status(404).send();
+            else
+                res.send({todo});
+        }).catch(e =>  res.status(400).send());
+    }
+});
+
 
 app.listen(port, () => {
     console.log('Stared on port ', port);

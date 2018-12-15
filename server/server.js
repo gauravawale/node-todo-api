0e63df79acc68c2c9c69669819a59d338e99e439
Awale, Gaurav <Gaurav.Awale@cdk.com>
2018-12-15 20:59:45 +0530

    Add DELETE /todo/:id route

--- server/server.js
+++ server/server.js
@@ -48,6 +48,21 @@ app.get('/todos/:id', (req, res) => {
     }
 });
 
+app.delete('/todos/:id', (req, res) => {
+   let id = req.params.id;
+
+   if(!ObjectID.isValid(id))
+       res.status(404).send();
+   else {
+       Todo.findByIdAndRemove(id).then((todo) => {
+          if (!todo)
+              res.status(404).send();
+          else
+              res.send({todo});
+       }).catch(e =>  res.status(400).send());
+   }
+});
+
 
 app.listen(port, () => {
     console.log('Stared on port ', port);

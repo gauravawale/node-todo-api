825a1c6a534be4806703b80a6b657e9bf606b0ab
Awale, Gaurav <Gaurav.Awale@cdk.com>
2018-12-15 22:07:42 +0530

    Add PATCH /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
-let express = require('express');
-let bodyParser = require('body-parser');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
 
 let {mongoose} = require('./db/mongoose');
@@ -63,6 +64,28 @@ app.delete('/todos/:id', (req, res) => {
    }
 });
 
+app.patch('/todos/:id', (req, res) => {
+    let id = req.params.id;
+    let body = _.pick(req.body, ['text', 'completed']);
+
+    if(!ObjectID.isValid(id))
+        return res.status(404).send();
+
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    }
+    else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+        if (!todo)
+            res.status(404).send();
+        else
+            res.send({todo});
+    }).catch(e =>  res.status(400).send());
+});
 
 app.listen(port, () => {
     console.log('Stared on port ', port);
